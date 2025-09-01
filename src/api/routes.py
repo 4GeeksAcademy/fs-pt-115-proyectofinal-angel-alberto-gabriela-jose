1acc8656fd373ba108ae273876970e456ac6e6f2
Angel-enc <enock8876@gmail.com>
2025-09-01 12:00:52 +0000

    especificado delete

--- src/api/routes.py
+++ src/api/routes.py
@@ -56,6 +56,7 @@ def register_user():
         db.session.rollback()
         return jsonify({"msg": "Error al guardar el usuario", "error": str(e)}), 500
 
+
 @api.route('/logout', methods=["POST"])
 @jwt_required()
 def logout():
@@ -63,22 +64,22 @@ def logout():
     user_id = get_jwt_identity()
     return jsonify({"msg": "Usuario desconectado"}), 200
 
-@api.route('/delete', methods=["DELETE"])
+
+@api.route('/delete-account', methods=["DELETE"])
 @jwt_required()
-def delete():
-    try: 
+def delete_account():
+    try:
         user_id = get_jwt_identity()
         user = User.query.get(user_id)
 
-        if not user: 
+        if not user:
             return jsonify({"msg": "Usuario no encontrado"}), 404
-        
+
         db.session.delete(user)
         db.session.commit()
 
         return jsonify({"msg": "Cuenta eliminada"}), 200
-    
+
     except Exception as e:
         db.session.rollback()
-        return jsonify({"msg": "Error al eliminar la cuenta", "error": str (e)}), 500
-    
+        return jsonify({"msg": "Error al eliminar la cuenta", "error": str(e)}), 500
