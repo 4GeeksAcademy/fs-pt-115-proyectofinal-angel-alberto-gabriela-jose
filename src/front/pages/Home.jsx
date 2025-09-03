0b8d032cf68d380beeb0b76e152841970ac5ff59
albertoZG <albertozg94@gmail.com>
2025-09-03 09:55:26 +0000

    registerrr

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
+			<h1 className="display-4">Hello Aura!!</h1>
 
 			< SingIn />
 
